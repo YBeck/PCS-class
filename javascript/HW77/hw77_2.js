6c16533af18880c6fc45b2b4d02d83558512428e
Yochanan Beck <ybeck115@gmail.com>
2017-10-25 20:35:15 -0400

    updated HW77 if !isNaN (a or b)

--- javascript/HW77/hw77_2.js
+++ javascript/HW77/hw77_2.js
@@ -3,7 +3,7 @@ var myApp = myApp || {};
 myApp.utils = (function (utils) {
     'use strict';
     utils.stringCaseInsensitiveEquals = function (a, b) {
-        if (!isNaN(a) && !isNaN(b)) {
+        if (!isNaN(a) || !isNaN(b)) {
             return "please enter a string";
         }
         return a.toUpperCase() === b.toUpperCase();
