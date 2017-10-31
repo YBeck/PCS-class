1a2798a774d457bbc2842e2f9808ed7f8947ed39
Yochanan Beck <ybeck115@gmail.com>
2017-10-31 17:06:18 -0400

    fixed map to take any callback

--- javascript/HW78-quiz/map.js
+++ javascript/HW78-quiz/map.js
@@ -1,12 +1,17 @@
 var originalArray = [2, 4, 6];
 
-function ourMap(arr) {
+function ourMap(arr, callback) {
     "use strict";
     var retArray = [];
     arr.forEach(function (element) {
-        retArray.push(element * 2);
+        retArray.push(callback(element));
     });
     return retArray;
 }
+
+function double(num) {
+    "use strict";
+    return num * 2;
+}
 console.log("array.map");
-console.log("original array", originalArray, "ourMap", ourMap(originalArray));
+console.log("original array", originalArray, "ourMap", ourMap(originalArray, double));
