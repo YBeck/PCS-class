be092aeaac62a3900caa1fbcd4d06cdcd5bc17b9
Yochanan Beck <ybeck115@gmail.com>
2018-01-17 12:27:38 -0500

    updated HW96 added apple

--- javascript/HW96/snake.js
+++ javascript/HW96/snake.js
@@ -1,28 +1,55 @@
 /*jshint -W104, -W119 */
 (function () {
     'use strict';
+    const setScore = document.getElementById('score');
+    let score = 0;
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     const img = document.createElement('img');
-    canvas.width = screen.width;
-    canvas.height = screen.height;
+    const apple = document.createElement('img');
+    resize();
+    
     // function log(event) {
     //     console.log(event);
     // }
+    function resize() {
+        canvas.width = window.innerWidth * 0.85;
+        canvas.height = window.innerHeight;
+    }
+    window.addEventListener('resize', resize);
+    class Apple{
+        constructor() {
+            //apple.src = 'images/apple.png';
+            this.placeApple();
+        } placeApple() {
+            const x = Apple.getRandomNumberBetween(0, (canvas.width)-64);
+            const y = Apple.getRandomNumberBetween(0, (canvas.height)-64);
+            this.appleX = x - (x % 64);
+            this.appleY = y - (y % 64);
+            apple.src = 'images/apple.png';
+            apple.onload = () => { ctx.drawImage(apple, this.appleX, this.appleY); };
+        } get x() {
+            return this.appleX;
+        } get y() {
+            return this.appleY;
+        } static getRandomNumberBetween(min, max) {
+            return Math.floor(Math.random() * (max - min) + min);
+        }
+    }
 
     class Snake{
         constructor() {
             this.x = 0;
             this.y = 0;
+            this.apple = new Apple();
         }
         createSnake(src, imgWidth, imgHeight) {
             img.src = src;
             this.imgWidth = imgWidth;
             this.imgHeight = imgHeight;
-            img.onload = function () { ctx.drawImage(img, this.x, this.y, imgWidth, imgHeight); };
+            img.onload = () =>{ ctx.drawImage(img, this.x, this.y, imgWidth, imgHeight); };
         }
         move() {  
-            //console.log(this.direction);
             switch (this.direction) {
                 case 'ArrowDown':
                     this.y += this.imgHeight;
@@ -43,22 +70,32 @@
             }
             if (this.x < 0) {
                 this.x = 0;
+                alert('You loose!');
+                clearInterval(game);
             } else if (this.x >= canvas.width){
                 this.x = canvas.width - this.imgWidth; 
+                alert('You loose!');
+                clearInterval(game);
             }
 
             if (this.y < 0) {
                 this.y = 0;
+                alert('You loose!');
+                clearInterval(game);
             } else if (this.y >= canvas.height) {
                 this.y = canvas.height - this.imgHeight; 
+                alert('You loose!');
+                clearInterval(game);
             }
             ctx.drawImage(img, this.x, this.y);
-
+            if (this.x === this.apple.x && this.y === this.apple.y) {
+                setScore.innerHTML = ++score;
+                this.apple.placeApple();
+            }
         }
         listener() {
             document.addEventListener('keydown', (event) => {
                 this.direction = event.code;
-                // console.log(this.direction);
             });
         }
     }
@@ -68,13 +105,9 @@
 
     //canvas.addEventListener('keydown', log);
     snake.listener();
-    setInterval(() => {
+    const game = setInterval(() => {
         ctx.clearRect(snake.x, snake.y, snake.imgWidth, snake.imgHeight);
         snake.move();
     }, 400);
-    
-    // document.addEventListener('keydown', (event) => {
-    //     snake.move(event);
-    // });
 
 })();
